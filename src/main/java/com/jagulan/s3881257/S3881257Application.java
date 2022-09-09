437a3126593e1597139718761fd57e0a1b43db1f
sJagulan <s3881257@student.rmit.edu.au>
2022-09-09 20:31:28 +1000

    first commit

---
+++ src/main/java/com/jagulan/s3881257/S3881257Application.java
@@ -0,0 +1,13 @@
+package com.jagulan.s3881257;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3881257Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3881257Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jagulan/s3881257/S3881257ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jagulan.s3881257;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3881257ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
